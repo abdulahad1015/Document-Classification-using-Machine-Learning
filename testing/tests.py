428b6f0ccd2c8cc8ce7c28ec88ef729bada1852f
Musaddiq <tankionlinemusaddiq@gmail.com>
2025-11-21 14:24:10 +0500

    naming convention fixedd

--- testing/tests.py
+++ testing/tests.py
@@ -49,19 +49,19 @@ def process_file(df, classification_function, api_name, options=None):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     log_results(f"{timestamp} | API: {api_name} | Accuracy: {accuracy:.4f} | Precision: {precision:.4f} | Recall: {recall:.4f} | F1 Score: {f1:.4f}")
 
-def slm_method(df):
+def SLM_method(df):
     """Process files and evaluate classifications using SLM."""
     # process_file(df, file_classification_slm, "SLM", options=get_options())
 
-def hdbscan_method(df):
+def HDBSCAN_method(df):
     """Process files and evaluate classifications using HDBSCAN."""
     process_file(df, file_classification_hdbscan, "HDBSCAN", options=get_options())
 
-def knn_method(df):
+def KNN_method(df):
     """Process files and evaluate classifications using KNN."""
     process_file(df, file_classification_knn, "KNN", options=get_options())
     
-def embeddings_method(df):
+def Embeddings_method(df):
     """Process files and evaluate classifications using Embeddings."""
     process_file(df, file_classification_embeddings, "Embeddings", options=get_options())
 
@@ -79,10 +79,10 @@ def evaluate_file_classification():
     if not {"File_name", "Classification"}.issubset(df.columns):
         raise ValueError("The Excel file must contain 'File_name' and 'Classification' columns.")
 
-    # slm_method(df)
-    hdbscan_method(df)
-    knn_method(df)
-    embeddings_method(df)
+    # SLM_method(df)
+    HDBSCAN_method(df)
+    KNN_method(df)
+    Embeddings_method(df)
     OneClassSvm_method(df)
     IsolationForest_method(df)
 
